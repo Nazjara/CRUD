b94f05c544b90e33148a79af4cafb02560a0255e
Nazar Lipkovskyy <nazar.lipkovskyy@gmail.com>
2018-04-16 14:29:12 +0300

    Add transactions

--- src/main/java/com/nazjara/util/HibernateUtil.java
+++ src/main/java/com/nazjara/util/HibernateUtil.java
@@ -1,47 +1,147 @@
 package com.nazjara.util;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+
 import java.io.Serializable;
 import java.util.List;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-
 @Repository
 public class HibernateUtil {
-	
-	@Autowired
+
+    @Autowired
     private SessionFactory sessionFactory;
-		
+
     public <T> Serializable create(final T entity) {
-        return sessionFactory.getCurrentSession().save(entity);        
+        Transaction transaction = null;
+        Session session = null;
+        Serializable savedEntity = null;
+        try {
+            session = sessionFactory.openSession();
+            Assert.notNull(session, "Hibernate session cannot be null");
+            transaction = session.beginTransaction();
+            savedEntity = session.save(entity);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (null != transaction) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+
+        return savedEntity;
     }
-    
+
     public <T> T update(final T entity) {
-        sessionFactory.getCurrentSession().update(entity);   
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            Assert.notNull(session, "Hibernate session cannot be null");
+            transaction = session.beginTransaction();
+            session.update(entity);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (null != transaction) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+
         return entity;
     }
-    
-	public <T> void delete(final T entity) {
-		sessionFactory.getCurrentSession().delete(entity);
-	}
-    
-    @SuppressWarnings("unchecked")	
-    public <T> List<T> fetchAll(Class<T> entityClass) {        
-        return sessionFactory.getCurrentSession().createQuery(" FROM "+entityClass.getName()).list();        
+
+    public <T> void delete(final T entity) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            Assert.notNull(session, "Hibernate session cannot be null");
+            transaction = session.beginTransaction();
+            session.delete(entity);
+            transaction.commit();
+
+        } catch (HibernateException e) {
+            if (null != transaction) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
     }
-  
-    @SuppressWarnings("rawtypes")
-	public <T> List fetchAll(String query) {        
-        return sessionFactory.getCurrentSession().createSQLQuery(query).list();        
-    }
-    
+
     @SuppressWarnings("unchecked")
-	public <T> T fetchById(Serializable id, Class<T> entityClass) {
-        return (T)sessionFactory.getCurrentSession().get(entityClass, id);
+    public <T> List<T> fetchAll(Class<T> entityClass) {
+        Transaction transaction = null;
+        Session session = null;
+        List list = null;
+        try {
+            session = sessionFactory.openSession();
+            Assert.notNull(session, "Hibernate session cannot be null");
+            transaction = session.beginTransaction();
+            list = session.createQuery(" FROM " + entityClass.getName()).list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (null != transaction) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+
+        return list;
     }
-    
-    
-	
+
+    @SuppressWarnings("rawtypes")
+    public <T> List fetchAll(String query) {
+        Transaction transaction = null;
+        Session session = null;
+        List<T> list = null;
+        try {
+            session = sessionFactory.openSession();
+            Assert.notNull(session, "Hibernate session cannot be null");
+            transaction = session.beginTransaction();
+            list = session.createSQLQuery(query).list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (null != transaction) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+
+        return list;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T fetchById(Serializable id, Class<T> entityClass) {
+        Transaction transaction = null;
+        Session session = null;
+        T entity = null;
+        try {
+            session = sessionFactory.openSession();
+            Assert.notNull(session, "Hibernate session cannot be null");
+            transaction = session.beginTransaction();
+            entity = (T) session.get(entityClass, id);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (null != transaction) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+
+        return entity;
+    }
+
+
 }
